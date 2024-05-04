477b541266c4c39d1fa9f97b8b3cb0d43f30807a
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-05-04 13:51:57 +0500

    Header

--- src/components/homes/home/index.jsx
+++ src/components/homes/home/index.jsx
@@ -1,33 +1,33 @@
-import React, { useEffect } from 'react';
-import { Footer, Header, Wrapper } from '../../../layout';
-import { animationCreate } from '../../../utils/utils';
-import BlogArea from './blog-area';
-import BrandArea from './brand-area';
-import CreativeArea from './creative-area';
-import HeroArea from './hero-area';
-import NewsLetter from './news-letter';
-import ProjectArea from './project-area';
-import ServicesArea from './services-area';
-import Testimonial from './testimonial';
+import React, { useEffect } from "react";
+import { Footer, Header, Wrapper } from "../../../layout";
+import { animationCreate } from "../../../utils/utils";
+// import BlogArea from "./blog-area";
+// import BrandArea from "./brand-area";
+// import CreativeArea from "./creative-area";
+// import HeroArea from "./hero-area";
+// import NewsLetter from "./news-letter";
+// import ProjectArea from "./project-area";
+// import ServicesArea from "./services-area";
+// import Testimonial from "./testimonial";
 
 const Home = () => {
   useEffect(() => {
     setTimeout(() => {
       animationCreate();
     }, 500);
-  }, [])
+  }, []);
   return (
     <Wrapper>
       <Header />
-      <HeroArea />
+      {/* <HeroArea />
       <CreativeArea/>
       <BrandArea/>
       <ServicesArea/>
       <ProjectArea/>
       <Testimonial/>
       <BlogArea/>
-      <NewsLetter/>
-      <Footer/>
+      <NewsLetter/> */}
+      {/* <Footer /> */}
     </Wrapper>
   );
 };
--- src/layout/headers/header.jsx
+++ src/layout/headers/header.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import Link from 'next/link';
-import useSticky from '../../hooks/use-sticky';
-import Sidebar from '../../components/common/off-canvas';
-import NavMenus from './nav-menus';
-import MobileMenu from './mobile-menu';
+import React from "react";
+import Link from "next/link";
+import useSticky from "../../hooks/use-sticky";
+import Sidebar from "../../components/common/off-canvas";
+import NavMenus from "./nav-menus";
+import MobileMenu from "./mobile-menu";
 
 const Header = () => {
   const { headerSticky } = useSticky();
@@ -11,17 +11,22 @@ const Header = () => {
   return (
     <React.Fragment>
       <header className="d-none d-lg-block">
-        <div id="header-sticky" className={`tp-header-area header-transparent pl-165 pr-165 pt-35 
-        ${headerSticky ? 'header-sticky' : ''}`}>
+        <div
+          id="header-sticky"
+          className={`tp-header-area header-transparent pl-165 pr-165 pt-35 
+        ${headerSticky ? "header-sticky" : ""}`}
+        >
           <div className="container-fluid">
             <div className="row align-items-center">
-              <div className="col-xl-3 col-lg-3">
+              {/* <div className="col-xl-3 col-lg-3">
                 <div className="tp-logo">
                   <Link href="/">
-                    <a><img src="/assets/img/logo/logo-blue.png" alt="" /></a>
+                    <a>
+                      <img src="/assets/img/logo/logo-blue.png" alt="" />
+                    </a>
                   </Link>
                 </div>
-              </div>
+              </div> */}
               <div className="col-xl-7 col-lg-7">
                 <div className="tp-main-menu">
                   <nav id="mobile-menu">
@@ -32,8 +37,13 @@ const Header = () => {
                 </div>
               </div>
               <div className="col-xl-2 col-lg-2">
-                <div className="tp-menu-bar text-end" onClick={() => setSidebarOpen(true)}>
-                  <button><i className="fal fa-bars"></i></button>
+                <div
+                  className="tp-menu-bar text-end"
+                  onClick={() => setSidebarOpen(true)}
+                >
+                  <button>
+                    <i className="fal fa-bars"></i>
+                  </button>
                 </div>
               </div>
             </div>
