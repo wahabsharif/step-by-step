5f8d6ee33d00e21f4797f061487153d50e67bb78
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-05-10 15:42:14 +0500

    About: Services Area

--- src/components/about/services-area.jsx
+++ src/components/about/services-area.jsx
@@ -1,5 +1,5 @@
-import Link from 'next/link';
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
 const ServicesArea = () => {
   return (
@@ -7,12 +7,54 @@ const ServicesArea = () => {
       <div className="ac-chose-area mb-130">
         <div className="container ac-chose-bg">
           <div className="row">
-            <ChoseItem duration='.3s' delay='.5s' icon='flaticon-group' title={<>Professional <br /> Team</>}
-              text={'24+ Team Member'} />
-            <ChoseItem duration='.5s' delay='.7s' item_num={'tpchosebox-three'} color="fea-color-5" icon='fas fa-star' title={<>Competitive <br /> Rate</>} text={'100% Client Satisfied'} />
-            <ChoseItem duration='.7s' delay='.9s' item_num={'tpchosebox-two'} color="fea-color-4" icon='flaticon-web' title={<>Certified <br /> Globally</>} text={'65.04 k Reach'} />
-            <ChoseItem duration='.9s' delay='1s' icon='fas fa-star' title={<>Competitive <br /> Rate</>}
-              text={'100% Client Satisfied'} />
+            <ChoseItem
+              duration=".3s"
+              delay=".5s"
+              icon="flaticon-group"
+              title={
+                <>
+                  Professional <br /> Team
+                </>
+              }
+              text={"24+ Team Member"}
+            />
+            <ChoseItem
+              duration=".5s"
+              delay=".7s"
+              item_num={"tpchosebox-three"}
+              color="fea-color-5"
+              icon="fas fa-star"
+              title={
+                <>
+                  Competitive <br /> Rate
+                </>
+              }
+              text={"100% Client Satisfied"}
+            />
+            <ChoseItem
+              duration=".7s"
+              delay=".9s"
+              item_num={"tpchosebox-two"}
+              color="fea-color-4"
+              icon="flaticon-web"
+              title={
+                <>
+                  Certified <br /> Globally
+                </>
+              }
+              text={"65.04 k Reach"}
+            />
+            <ChoseItem
+              duration=".9s"
+              delay="1s"
+              icon="fas fa-street-view"
+              title={
+                <>
+                  At Your <br /> Door Step
+                </>
+              }
+              text={"24/7 Support"}
+            />
           </div>
         </div>
       </div>
@@ -24,12 +66,17 @@ export default ServicesArea;
 
 const ChoseItem = ({ duration, delay, item_num, icon, title, text, color }) => {
   return (
-    <div className="col-xl-3 col-lg-6 col-md-6 col-12 wow tpfadeUp"
-      data-wow-duration={duration} data-wow-delay={delay}>
+    <div
+      className="col-xl-3 col-lg-6 col-md-6 col-12 wow tpfadeUp"
+      data-wow-duration={duration}
+      data-wow-delay={delay}
+    >
       <div className="tp-chose-item mb-30">
         <div className={`tpchosebox ${item_num && item_num}`}>
           <div className={`tpchosebox__icon ${color && color} mb-30`}>
-            <a href="#"><i className={icon}></i></a>
+            <a href="#">
+              <i className={icon}></i>
+            </a>
           </div>
           <div className="tpchosebox__content">
             <h4>
@@ -42,5 +89,5 @@ const ChoseItem = ({ duration, delay, item_num, icon, title, text, color }) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
