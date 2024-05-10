9e4224baf93cbf26b2d25c537e872b74bfbb5fca
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-05-10 18:59:06 +0500

    Handyman Updated

--- src/components/handyman/handyman-services-data.js
+++ src/components/handyman/handyman-services-data.js
@@ -5,7 +5,7 @@ const services_data = [
     id: 1,
     duration: ".3s",
     delay: ".5s",
-    img: "/assets/img/service/handyman-curtain-installation.png",
+    img: "/assets/img/service/handyman/handyman-curtain-installation.png",
     title: "Curtain Installation",
     border_effect: "",
     text_1:
@@ -15,7 +15,7 @@ const services_data = [
     id: 2,
     duration: ".5s",
     delay: ".7s",
-    img: "/assets/img/service/handyman-emergency-home-repair.png",
+    img: "/assets/img/service/handyman/handyman-emergency-home-repair.png",
     title: "Emergency Home Repair",
     border_effect: "sv-border-effect-1",
     text_1:
@@ -25,7 +25,7 @@ const services_data = [
     id: 3,
     duration: ".7s",
     delay: ".9s",
-    img: "/assets/img/service/handyman-floating-shelves-hanging.png",
+    img: "/assets/img/service/handyman/handyman-floating-shelves-hanging.png",
     title: "Floating Shelves Hanging",
     border_effect: "sv-border-effect-2",
     text_1:
@@ -35,7 +35,7 @@ const services_data = [
     id: 4,
     duration: ".9s",
     delay: "1s",
-    img: "/assets/img/service/handyman-home-maintenance.png",
+    img: "/assets/img/service/handyman/handyman-home-maintenance.png",
     title: "Home Maintenance",
     border_effect: "sv-border-effect-3",
     text_1:
@@ -46,7 +46,7 @@ const services_data = [
     id: 5,
     duration: ".9s",
     delay: "1.1s",
-    img: "/assets/img/service/handyman-tv-wall-bracket.png",
+    img: "/assets/img/service/handyman/handyman-tv-wall-bracket.png",
     title: "TV Wall Bracket",
     border_effect: "sv-border-effect-4",
     text_1:
@@ -56,7 +56,7 @@ const services_data = [
     id: 6,
     duration: "1s",
     delay: "1.1s",
-    img: "/assets/img/service/handyman-local-handyman.png",
+    img: "/assets/img/service/handyman/handyman-local-handyman.png",
     title: "Local Handyman",
     border_effect: "sv-border-effect-5",
     text_1:
