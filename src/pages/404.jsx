3056163ffdaf5ecfd7b5c36b1f76dd33f70f783f
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-05-10 16:51:51 +0500

    error 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
-import Breadcrumb from '../components/common/breadcrumb/breadcrumb';
-import SEO from '../components/seo';
-import { FooterThree, HeaderSix, Wrapper } from '../layout';
+import React from "react";
+import Breadcrumb from "../components/common/breadcrumb/breadcrumb";
+import SEO from "../components/seo";
+import { FooterThree, Header, Wrapper } from "../layout";
+import Image from "next/image";
 
 const ErrorPage = () => {
   return (
     <Wrapper>
-      <SEO pageTitle={'Not Found'} />
-      <HeaderSix />
-      <Breadcrumb title={'404 Page'} back_home={true} />
+      <SEO pageTitle={"Not Found"} />
+      <Header />
+      <Breadcrumb title={"404 Page"} back_home={true} />
       {/* <!--404-area-start --> */}
       <div className="tp-404-area tp-404-circle sky-bg pt-200 pb-150 fix">
         <div className="container">
           <div className="row">
             <div className="col-12">
               <div className="tp-404-thumb d-flex justify-content-center align-items-center z-index-1">
-                <img src="/assets/img/contact/404.png" alt="" />
+                <Image
+                  src="/assets/img/contact/404.png"
+                  width={450}
+                  height={450}
+                  alt="Error 404"
+                />
               </div>
             </div>
           </div>
